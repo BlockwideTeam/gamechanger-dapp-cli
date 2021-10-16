0e86eaf4249588d00a712daa2e97de0b262b80e4
zxpectre <15870846+zxpectre@users.noreply.github.com>
2021-10-16 20:52:09 -0300

    add test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import execa from 'execa';
 
-// test('main', async t => {
-// 	const {stdout} = await execa('./cli.js', ['ponies']);
-// 	t.is(stdout, 'ponies & rainbows');
-// });
+test('main', async t => {
+	const {stdout} = await execa('./cli.js', ['mainnet','build', 'url' ,'-f' ,'demo.gcs']);
+	t.is(stdout, 'https://wallet.gamechanger.finance/api/1/tx/woTCpHR5cGXConR4wqV0aXRsZcKkRGVtb8KrZGVzY3JpcMSKb27DmSHEmGVhdGVkIHfEi2ggZ2FtZWNoYW5nZXItZGFwcC1jbGnCqMSudGHEuMWCwoHCozEyM8KBwqfErnNzYcS0wqxIZWxsbyBXb3JsZCE');
+});

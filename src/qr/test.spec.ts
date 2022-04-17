ff194b5ed855f7cb827c70307faaebe93d64cf78
Ribó <elribonazo@gmail.com>
2022-04-17 22:21:06 +0200

    Improve test coverage + debugging.

--- src/qr/test.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('GameChanger DAPP CLI', function () {
-	it('should build the correct url for the demo.gcs script', async function () {
-		await new Promise((resolve) => {
-			setTimeout(() => {
-				resolve(null);
-			}, 10);
-		});
-	});
-});
